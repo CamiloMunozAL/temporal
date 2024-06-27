2fac2cbba992e5eb51ab64f40f632c326388a628
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-06-27 06:27:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Fragment } from 'react'
+import ViewDetails from './components/ViewDetails'
+
+let movieExample = {
+  title: 'The Dark Knight',
+  year: 2008,
+  director: 'Christopher Nolan',
+  description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+  img: 'https://m.media-amazon.com/images/S/pv-target-images/e9a43e647b2ca70e75a3c0af046c4dfdcd712380889779cbdc2c57d94ab63902.jpg'
+};
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <ViewDetails 
+      title={movieExample.title}
+      year={movieExample.year}
+      director={movieExample.director}
+      description={movieExample.description}
+      img={movieExample.img}
+      />
+    </>
+  )
+}
+
+export default App
